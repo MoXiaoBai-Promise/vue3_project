5c26181e12a2af7a84338c3086f039f0bdaf7ed3
moxiaobai <1123806041@qq.com>
2024-05-10 22:51:26 +0800

    510

--- src/setting.ts
+++ src/setting.ts
@@ -1,7 +1,7 @@
 //项目logo和标题，可以在此修改
 export default {
     title: '莫小白Vue3项目',
-    logo: '../../../../public/logo.png',
+    logo: '../../../../public/ベジータ.png',
     //是否需要logo组件true|false
     logoHide: true
 }
